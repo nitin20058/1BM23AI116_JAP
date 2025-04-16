b1c138b92f0eaad6937fc4a7eda1adfb6136ec18
nitin20058 <nitinravikumar.ai23@bmsce.ac.in>
2025-04-16 15:36:06 +0530

    Add files via upload

---
+++ membership.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class membership {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the Gym");
+        System.out.println("Enter number of members:");
+        int num = input.nextInt();
+
+
+        input.nextLine();
+
+        for (int i = 0; i < num; i++) {
+            System.out.println("Enter your name");
+            String name = input.nextLine();
+
+            System.out.println("Enter your age:");
+            int age = input.nextInt();
+            input.nextLine();
+            System.out.println("Enter your membership Status:");
+            boolean status = input.nextBoolean();
+            System.out.println("Enter your membership duration:");
+            int months = input.nextInt();
+            input.nextLine();
+            float price = months * 1000.00f;
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+            System.out.println("Status: " + status);
+            System.out.println("Gym fee is " + price + " for " + months + " months");
+        }
+
+    }
+}
+//GYM OUTPUT:
+//
+//Welcome to the Membership
+//Enter your name
+// monish
+//Enter the age
+//19
+//Active member? type (true/false)
+//true
+//Enter the duration in Months
+//6
+//Details:
+//Name  monish
+//Age 19
+//Duration 6
+//Fee 300.0
+//
+//Process finished with exit code 0
